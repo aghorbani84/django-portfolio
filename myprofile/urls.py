10c3bc5d39116b0a1fce6930a0f0baee56751c99
Abolfazl Ghorbani | ابوالفضل قربانی <aghorbani@tuta.io>
2024-03-09 16:22:41 +0000

    Update urls.py

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from .views import signup_view
+from .views import login_view
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    
 ]
